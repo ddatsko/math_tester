2fc89dc06ebb274f9378762d5e670e874ed34e2c
Denys Datsko <35429810+ddatsko@users.noreply.github.com>
2019-02-27 12:06:24 +0200

    Create MathLy_API_example.py

---
+++ examples/MathLy_API_example.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+import requests
+raw_url = "https://math.ly/api/v1/"
+subject = "algebra/"
+problem_type = "linear-equations"
+difficulty = "beginner"
+url = raw_url + subject + problem_type + ".json?difficulty=" + difficulty
+result = requests.get(url)
+pprint(result.json())
