69857208b757bba9641a8390f8522d2ff623a8c2
ddatsko <datsko.d@ucu.edu.ua>
2019-02-27 13:24:17 +0200

    added latex2pdf converter

---
+++ tmp/latex2pdf.py
@@ -0,0 +1,12 @@
+from latex import build_pdf
+
+def latex2pdf(data, output):
+    """
+    (str, str) -> None
+    :param data: string of LaTeX document
+    :param output: name of output file
+    :return: None
+    """
+    pdf = build_pdf(data)
+    with open(output, "wb") as file:
+        pdf.save_to(file)
